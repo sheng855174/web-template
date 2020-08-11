b1512b74335aec46b5ce5d4d3648143106378a74
sheng855174 <sheng855174@gmail.com>
2020-08-12 07:24:12 +0800

    init

---
+++ src/main/java/tw/sheng/config/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+package tw.sheng.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers(
+                            "/registration**",
+                            "/js/**",
+                            "/css/**",
+                            "/img/**",
+                            "/scss/**",
+                            "/vendor/**",
+                            "/**").permitAll()
+                    .antMatchers("/**").access("hasRole('ADMIN') or hasRole('USER')")
+                .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                        .loginPage("/login")
+                            .permitAll()
+                .and()
+                    .logout()
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login?logout")
+                .permitAll();
+        http.csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN", "USER")
+                .and()
+                .withUser("user").password("user12345").roles("USER");
+    }
+
+}
---
+++ src/main/java/tw/sheng/controller/LoginController.java
@@ -0,0 +1,56 @@
+package tw.sheng.controller;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = {"/","/index"}, method = {RequestMethod.POST, RequestMethod.GET})
+    public String index(HttpServletRequest request){
+        return "index";
+    }
+
+    @RequestMapping(value = {"/blank"}, method = {RequestMethod.POST, RequestMethod.GET})
+    public String blank(HttpServletRequest request){
+        return "blank";
+    }
+
+    @RequestMapping(value = {"/buttons"}, method = {RequestMethod.POST, RequestMethod.GET})
+    public String buttons(HttpServletRequest request){
+        return "buttons";
+    }
+
+    @RequestMapping(value = {"/cards"}, method = {RequestMethod.POST, RequestMethod.GET})
+    public String cards(HttpServletRequest request){
+        return "cards";
+    }
+
+    @RequestMapping(value = {"/charts"}, method = {RequestMethod.POST, RequestMethod.GET})
+    public String charts(HttpServletRequest request){
+        return "charts";
+    }
+
+    @RequestMapping(value = {"/tables"}, method = {RequestMethod.POST, RequestMethod.GET})
+    public String tables(HttpServletRequest request){
+        return "tables";
+    }
+
+    @RequestMapping(value = {"/login"}, method = {RequestMethod.POST, RequestMethod.GET})
+    public String login(HttpServletRequest request){
+        return "login";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping(value = {"/404"}, method = {RequestMethod.POST, RequestMethod.GET})
+    public String Html404(HttpServletRequest request){
+        return "404";
+    }
+
+}
